c895496ede2e930c03f0f6991788295c5aa2b17d
Avi Levin <avi@milyli.com>
2020-01-25 19:09:17 +0200

    Initial commit

---
+++ Exercise1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+/*
+Classic producer/consumer problem.
+As I mentioned in the interview, I'm not a threading expert, but I remembered Concurrent Collections... and
+this website http://www.albahari.com/threading/part5.aspx#_BlockingCollectionT
+
+I figured TPL DataFlow was overkill. Probably could have used async somewhere, and added graceful exit code,
+but decided not worth it for the exercise.
+*/
+    internal class Program
+    {
+        static void Main()
+        {
+            BlockingCollection<int> queue = new BlockingCollection<int>(new ConcurrentQueue<int>());
+            
+            for (int i = 0; i < 5; i++)
+            {
+                // name the clerk
+                var clerkId = (char)(65+i);
+                Task.Factory.StartNew (() => ClerkWork(clerkId));
+            }
+    
+            for (int shopperId = 1;; shopperId++)
+            {
+                Console.WriteLine($"{shopperId} getting in line");
+                queue.Add(shopperId);
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+    
+            void ClerkWork(char clerkId)
+            {
+                var random = new Random();
+                Console.WriteLine($"{clerkId} ready for work");
+                foreach (var shopperId in queue.GetConsumingEnumerable())
+                {
+                    var wait = TimeSpan.FromMilliseconds(random.Next(1000,5000));
+                    Console.WriteLine($"{clerkId} helping shopper {shopperId}");
+                    Thread.Sleep(wait);
+                    Console.WriteLine($"{clerkId} done helping {shopperId}");
+                }
+            }
+        }
+    }
+}
---
+++ Exercise2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exercise2
+{
+/*
+Nice to get a repeat question from the first interview. This is based on what was the "correct" solution then.
+*/
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (!args.Any())
+            {
+                Console.WriteLine("Specify file path");
+                args = new[] {Console.ReadLine()};
+            }
+
+            string filePath = args[0];
+            Console.WriteLine("Initializing...");
+            // ready file lazily and group by sort key
+            var lookup = File.ReadLines(filePath).ToLookup(GetSortKey);
+            
+            // allow reuse of lookup after initial creation
+            while (true)
+            {
+                Console.Write("Enter input: ");
+                var input = Console.ReadLine();
+
+                Console.WriteLine();
+                Console.WriteLine($"Matches for {input}:");
+                foreach (var match in lookup[GetSortKey(input)])
+                {
+                    Console.WriteLine(match);
+                }
+            }
+        }
+
+        // Sort the characters so that the ordering is irrelevant.
+        // NOTE: It should be possible to make this a tiny bit faster by using some
+        // other method to turn the sorted char array into a comparable key.
+        static string GetSortKey(string str)
+        {
+            return new string(str.OrderBy(s => s).ToArray());
+        }
+    }
+}
